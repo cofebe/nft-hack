a246bea927a2f5ca8c2ab094638ce49b44c02f71
Bryan Carter <deloreanz@gmail.com>
2022-01-14 23:22:18 -0800

    added mode to switch contexts

--- src/App.js
+++ src/App.js
@@ -2,41 +2,46 @@ import { useEffect, useState } from 'react';
 import logo from './eth-diamond-rainbow.png';
 import videojs from 'video.js'
 import './App.css';
+import {
+  Button,
+  Typography,
+} from '@mui/material';
 import { Client, isSupported } from '@livepeer/webrtmp-sdk';
 
 if (!isSupported()) {
-  alert('webrtmp-sdk is not currently supported on this browser')
+  alert('webrtmp-sdk is not currently supported on this browser');
 }
 
 const client = new Client();
 
 async function start() {
-  const streamKey = '3fc3-4kb3-1oyi-9564'
+  const streamKey = '3fc3-4kb3-1oyi-9564';
 
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
-  })
+  });
 
-  const session = client.cast(stream, streamKey)
+  const session = client.cast(stream, streamKey);
 
   session.on('open', () => {
-    console.log('Stream started.')
-  })
+    console.log('Stream started.');
+  });
 
   session.on('close', () => {
-    console.log('Stream stopped.')
-  })
+    console.log('Stream stopped.');
+  });
 
   session.on('error', (err) => {
-    console.log('Stream error.', err.message)
-  })
+    console.log('Stream error.', err.message);
+  });
 }
 
 // start streaming to livepeer
 // start();
 
 function App() {
+  const [mode, setMode] = useState('home');
 
   useEffect(() => {
 
@@ -52,9 +57,24 @@ function App() {
         {/* <video  src="https://cdn.livepeer.com/hls/3fc3wygcixo3kwps/index.m3u8" controls autoplay></video> */}
       </header>
 
+      {mode === 'home' &&
+        <Button
+          variant='contained'
+          onClick={() => setMode('stream')}
+        >Start Stream</Button>
+      }
+
+      <div>
+        {mode === 'stream' &&
+          <div>Start Stream</div>
+        }
+      </div>
+
       <video data-setup='{}'>
         <source src="https://cdn.livepeer.com/hls/3fc3wygcixo3kwps/index.m3u8" type="application/x-mpegURL"/>
       </video>
+
+
     </div>
   );
 }
