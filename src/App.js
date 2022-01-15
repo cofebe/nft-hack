6e2cc110565e8f53352537cc67784798d0181940
Bryan Carter <deloreanz@gmail.com>
2022-01-14 20:02:09 -0800

    added livepeer SDK to stream RTMP from browser directly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react';
 import logo from './eth-diamond-rainbow.png';
 import './App.css';
+import { Client, isSupported } from '@livepeer/webrtmp-sdk';
+
+if (!isSupported()) {
+  alert('webrtmp-sdk is not currently supported on this browser')
+}
+
+const client = new Client();
+
+async function start() {
+  const streamKey = '3fc3-4kb3-1oyi-9564'
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  })
+
+  const session = client.cast(stream, streamKey)
+
+  session.on('open', () => {
+    console.log('Stream started.')
+  })
+
+  session.on('close', () => {
+    console.log('Stream stopped.')
+  })
+
+  session.on('error', (err) => {
+    console.log('Stream error.', err.message)
+  })
+}
+
+// start streaming to livepeer
+// start();
 
 function App() {
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +48,7 @@ function App() {
         <p>
           NFT HACK
         </p>
+        {/* <video  src="https://cdn.livepeer.com/hls/3fc3wygcixo3kwps/index.m3u8" controls autoplay></video> */}
       </header>
     </div>
   );
