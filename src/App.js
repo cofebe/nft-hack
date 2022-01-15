c10957064e2f114e4a895d28226363035db99663
Yari <ycuevas@cofebe.com>
2022-01-15 07:53:53 -0800

    Add contract use

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ const streamPlaybackUrl = 'https://cdn.livepeer.com/hls/26cafzyg7i8yhgb5/index.m
 function App() {
   const { loginProvider, signer, address, account, accounts, connect, isConnected, balances: coinBalances, network, networkType, networkId, getNetwork } = useWallet();
   const [mode, setMode] = useState('home');
-  
+
+  const contract = ERC721Contract({contractAddress: address, loginProvider: signer})
+  console.log(contract)
   useEffect(() => {
 
   }, []);
