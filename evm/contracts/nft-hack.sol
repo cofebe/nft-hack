4229352154a24d8582e19a93ddf70ed062c2d70a
Bryan Carter <deloreanz@gmail.com>
2022-01-14 21:11:07 -0800

    first rev of contract

--- evm/contracts/nft-hack.sol
+++ evm/contracts/nft-hack.sol
@@ -7,6 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract NFTHACK {
   
   address adminAddress;
+  // streams[NFT_CONTRACT_ADDRESS][HOST_ADDRESS] = Stream
+  mapping(address => mapping(address => Stream)) streams;
+
+
+  struct Stream {
+    address host;
+    address requiredCollection;
+    string url;
+  }
+
+  event StreamCreated(address host, address requiredCollection, string url);
 
   modifier adminOnly {
     require(msg.sender == adminAddress);
@@ -15,6 +26,17 @@ contract NFTHACK {
 
   constructor() {
     adminAddress = msg.sender;
+  }
+
+  function createStream(address requiredCollection, string calldata url) public returns (bool) {
+    Stream memory newStream = Stream({
+      host: msg.sender,
+      requiredCollection: requiredCollection,
+      url: url
+    });
+    streams[requiredCollection][msg.sender] = newStream;
+    emit StreamCreated(msg.sender, requiredCollection, url);
+    return true;
   }
 
 
