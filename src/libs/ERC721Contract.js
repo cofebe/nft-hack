cdcc0d7c04182353ef085e97c68e98a7c3aa0b89
Yari <ycuevas@cofebe.com>
2022-01-15 07:44:46 -0800

    update evm folder placement

--- src/libs/ERC721Contract.js
+++ src/libs/ERC721Contract.js
@@ -1,4 +1,4 @@
-import ERC721 from '../evm/artifacts/@openzeppelin/contracts/token/erc721/erc721.sol/ERC721.json';
+import ERC721 from '../evm/artifacts/@openzeppelin/contracts/token/ERC721/ERC721.sol/ERC721.json'
 import { Contract } from 'ethers';
 export default ({ contractAddress, loginProvider}) => {
   const provider = loginProvider;
