fe048d298392f458127f56a308e3c73766d165cb
Bryan Carter <deloreanz@gmail.com>
2022-01-15 20:10:43 -0800

    fixing contract ref

--- src/libs/nftHackContract.js
+++ src/libs/nftHackContract.js
@@ -1,6 +1,6 @@
 import { Contract, providers } from 'ethers';
 // import { getProvider } from './web3Providers.js';
-import NftHack from '../evm/artifacts/contracts/main.sol/NFTHACK.json';
+import NftHack from '../evm/artifacts/contracts/nft-hack.sol/NFTHACK.json';
 
 export default ({ contractAddress, networkId, loginProvider, config, }) => {
   return new Contract(contractAddress, NftHack.abi, loginProvider);
